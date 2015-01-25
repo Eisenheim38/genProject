9dcd6351594dc87eef31ed900cd9c434a7579ba9
Carlos Avilan <avilancj@gmail.com>
2015-01-25 14:34:53 -0430

    Cambios pequeños en cross.py y creación de __initPopulation en AG

--- mods/AG.py
+++ mods/AG.py
@@ -1,9 +1,35 @@
 from selection import *
 from mutation import *
 from cross import *
+import random
 
 class Proy:
   """The place where the magic is."""
-  def __init__(self):
-    print 'Class created.'
+  #Class variables
+  cromosomas = []
+  zombies = []
+  width = 0
+  height = 0
+  tam = 0
+
+  #Class initializer
+  #needs int w, int h, list of int z
+  def __init__(self,w,h,z):
+    self.width=w
+    self.height=h
+    self.zombies=z
+
+  #Creates population
+  def __initPopulation(self,t):
+    for i in range(t):
+      gen = []
+      for j in range(4):
+        gen.append([random.randint(0,3) for i in range(4)])
+      self.cromosomas.append(gen)
+
+  def simulate(self,t):
+    self.__initPopulation(t)
+    print self.cromosomas
+
+
 		
--- mods/cross.py
+++ mods/cross.py
@@ -1,23 +1,25 @@
-import random.py
+import random
+
 #modulo de cruce, recibe poblacion retorna cruce de poblacion
-def cruce(poblacion,pc): #poblacion= individuos a cruzar , pc=probabilidad de cruce preferiblemente 0.7
+#poblacion= individuos a cruzar , pc=probabilidad de cruce preferiblemente 0.7
+def cruce(poblacion,pc): 
 	nueva_poblacion=[[[]]]
 	while (len(poblacion)>0):
-		if len(poblacion)==1 #solo queda un individuo no es posible cruzarlo
+		if len(poblacion)==1: #solo queda un individuo no es posible cruzarlo
 			nueva_poblacion.append(poblacion[0])
-		else
+		else:
 		 	pareja1=poblacion[random.randint(0,len(poblacion)-1)] #seleccionamos la pareja 1 aleatoriamente
 		 	poblacion.remove(pareja1);								#ya este individuo esta fuera de nuestra poblacion de cruce
 			pareja2=poblacion[random.randint(0,len(poblacion)-1)] #seleccionamos la pareja 2 aleatoriamente
 			poblacion.remove(pareja2);								#ya este individuo esta fuera de nuestra poblacion de cruce
-			if random.random() <= pc #se calcula la probabilidad de cruce
+			if random.random() <= pc: #se calcula la probabilidad de cruce
 				a=len(pareja1)
 				n=a/2
 				np1=pareja1[0:n]+pareja2[n:a]
 				np2=pareja2[0:n]+pareja1[n:a]   #se da cruce
 				nueva_poblacion.append(np1)
 				nueva_poblacion.append(np2)
-			else
+			else:
 				nueva_poblacion.append(pareja1)  # no hay cruce se clonan los individuos
 				nueva_poblacion.append(pareja2)  
 	return nueva_poblacion #se ha creado una nueva poblacion 
