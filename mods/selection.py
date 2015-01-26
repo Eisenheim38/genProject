d4b1e30c33479506ec540dd3c961816015e39b35
mauricio208 <mauricio208@gmail.com>
2015-01-26 18:50:05 -0430

    Selection algoritmo de adaptabilidad por gen

---
+++ mods/selection.py
@@ -0,0 +1,10 @@
+def apt_gen(cromosoma,w,h,zt,n):
+	wf=(w-4)/2
+	z=zt[i]*4 #vida zombie total
+	p=cromosoma.count(3)#potato-mine
+	r=cromosoma.count(2)#repeater
+	n=cromosoma.count(1)#peashooter
+	damage=((r*2)+n)*wf
+	zombie_survivors=damage-z
+	dz=zombie_survivors+(p*4)#no deberia ser -(p*4)?
+	return dz
