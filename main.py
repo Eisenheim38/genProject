bc5612e83b06c2c436980346ad6ded7ac4682646
Carlos Avilan <avilancj@gmail.com>
2015-01-28 18:05:41 -0430

    Primer acople con el cruce y detalles varios

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from mods.AG import *
+from sys import stdin
 
 def main():
-  Proy()
-  #print 'I\'m the main'
+  """
+  w = int(raw_input())
+  h = int(raw_input())
+  zi = int(raw_input())
+  """
+  whz = map(int,stdin.readline().split())
+  zf = [0]*(whz[1]+1)
+  z = map(int,stdin.readline().split())
+  for i in z:
+    zf[i] = zf[i]+1
+  #p = Proy(10,1,[1,1,1])
+  p = Proy(whz[0],whz[1],z)
+  p.simulate(10)
  
 if __name__=='__main__':
   main()
--- mods/mutation.py
+++ mods/mutation.py
@@ -6,7 +6,7 @@ def mutar(poblacion,pm):
 		if ( random.random()<= pm ): #por cada individuo se chequea si existe la probabilidad de que mute
 			gen=random.randint(0,len(cromosoma)) #el gen que muta es seleccionado aleatoriamente
 			alelos=xrange(0,4) #posibles alelos a mutar (posiciones de la fila(gen))
-			for x in xrange(0,random.randint(1,4):	#la cantidad de alelos a mutar es aleatoria
+			for x in xrange(0,random.randint(1,4)):	#la cantidad de alelos a mutar es aleatoria
 				mut=random.choice(alelos) #se escoge cual alelo mutar aleatoriamente
 				alelos.remove(mut); #se extrae dicho alelo para no ser seleccionado en caso de que se mute mas de uno
 				l_alelo=xrange(0,4).remove(cromosoma[gen][mut]) # el valor debe ser cualquier exepto el actual
