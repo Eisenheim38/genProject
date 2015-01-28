c4f39cdd6146f43b7e484ccc9b16079c07e3cb65
mauricio208 <mauricio208@gmail.com>
2015-01-27 23:22:33 -0430

    Funcion alternativa aun no es la simulacion sigue siendo una aproximacion

--- mods/selection.py
+++ mods/selection.py
@@ -1,9 +1,21 @@
-def apt_gen(cromosoma,w,h,zt):
+def apt_gen_aprox(cromosoma,w,zt):
 	wf=(w-4)/2
-	z=zt[i]*4 #vida zombie total
+	z=zt[i]*8 #vida zombie total
 	p=cromosoma.count(3)#potato-mine
 	r=cromosoma.count(2)#repeater
 	n=cromosoma.count(1)#peashooter
-	damage=((r*2)+n)*wf+(p*4)
+	damage=((r*2)+n)*wf+(p*8)
 	zombie_survivors=damage-z
 	return zombie_survivors
+
+def apt_simul_aprox(cromosoma,w,zt):
+	z=zt[i]*8 #vida zombie total
+	p=cromosoma.count(3)#potato-mine
+	r=cromosoma.count(2)#repeater
+	n=cromosoma.count(1)#peashooter
+	seed_damage=n+(r*2)
+	for i in xrange(0,w-4):
+		zombie_survivors=zombie_survivors-seed_damage
+	if (zombie_survivors > 0):
+		zombie_survivors=zombie_survivors-(seed_damage+(p*8))					
+	return zombie_survivors # si el resultado es negativo el gen se adapta mejor
