dcbcc43ebfb7203ee7ef3dd9e7bfefaf179ba9ed
mauricio208 <mauricio208@gmail.com>
2015-01-28 19:48:04 -0430

    ahora si deberia

--- mods/mutation.py
+++ mods/mutation.py
@@ -9,9 +9,9 @@ def mutar(poblacion,pm):
 			for x in range(1,random.randint(2,4)):	#la cantidad de alelos a mutar es aleatoria
 				mut=random.choice(alelos) #se escoge cual alelo mutar aleatoriamente
 				alelos.remove(mut); #se extrae dicho alelo para no ser seleccionado en caso de que se mute mas de uno
-				l_alelo=range(0,4).remove(cromosoma[gen][mut]) # el valor debe ser cualquier excepto el actual
-				if l_alelo:
-					cromosoma[gen][mut]=random.choice(l_alelo)# se muta finalmente
+				l_alelo=range(0,4)
+				l_alelo.remove(cromosoma[gen][mut]) # el valor debe ser cualquier excepto el actual
+				cromosoma[gen][mut]=random.choice(l_alelo)# se muta finalmente
 	return poblacion
 
 
